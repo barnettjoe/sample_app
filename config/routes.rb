8e67f2f341873ef4c0df1491571d311b8fba1da6
barnettjoe <joebarnett876@gmail.com>
2017-09-08 11:05:57 +0100

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #root
   root 'static_pages#home'
+
+  # static pages
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  # users
+  get '/signup', to: 'users#new'
+
 end
